dba1d2245f66657b1e5f2b3367f9dc1a6a8a4a47
ajaypandhavale <aj40135821@gmail.com>
2025-02-04 16:44:00 +0530

    pushing code to update but not main branch

--- Demo7.java
+++ Demo7.java
@@ -20,6 +20,7 @@ public static void main(String[] args){
 
     System.out.println("pushing code to main branch 3");
 
+    System.out.println("pushing code to update but not to main branch");
 
 }
 
