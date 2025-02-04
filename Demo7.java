28c035f96e173917f767af97c32cbd501652525c
ajaypandhavale <aj40135821@gmail.com>
2025-02-04 20:03:26 +0530

    Revert "changing using branch"
    This reverts commit 4ca11751

--- Demo7.java
+++ Demo7.java
@@ -2,39 +2,6 @@ public class Demo7 {
 public static void main(String[] args){
 
     System.out.println("Hii, Demo7");
-
-    System.out.println("updating using branch");
-
-    System.out.println("updating to ignore2");
-
-    System.out.println("updating to repo ");
-
-    System.out.println("pushing code to repo");
-
-    System.out.println("pulling from repo ");
-
-    System.out.println("pushing code to main branch");
-
-    System.out.println("pushing code to main branch 1");
-
-    System.out.println("pushing code to main branch 2");
-
-    System.out.println("pushing code to main branch 3");
-
-    System.out.println("pushing code to update but not to main branch");
-
-    System.out.println("pushing code to update also to main branch");
-
-    System.out.println("pushing code to update but not to main branch 2");
-
-    System.out.println("pushing code to update but not to main branch 3");
-
-    System.out.println("pushing code to update but not to main branch 4");
-
-    System.out.println("pushing code to update but not to main branch 5");
-
-
-    System.out.println("CHANGING BRANCH");
 }
 
 
